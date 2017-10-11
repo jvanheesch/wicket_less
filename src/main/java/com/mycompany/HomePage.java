7c7df91fdcc0f6748f63698d5e9b46f6aaefb076
Joris van Heesch <joris.vanheesch@hpcds.com>
2017-10-11 19:29:08 +0200

    Example of cascading

--- src/main/java/com/mycompany/HomePage.java
+++ src/main/java/com/mycompany/HomePage.java
@@ -8,10 +8,28 @@ import org.apache.wicket.markup.html.WebPage;
 public class HomePage extends WebPage {
     private static final long serialVersionUID = -8896769873107976128L;
 
+    /**
+     * NOTE: THE ORDER IS OF IMPORTANCE HERE! (CASCADING Style Sheets)
+     * <p>
+     * Both css files seem to be compiled independently.
+     * More.less results in:
+     * h1 {
+     * color: #00ff00;
+     * }
+     * h2 {
+     * color: #00ff00;
+     * }
+     * and HomePage.less results in:
+     * h1 {
+     * color: #ff0000;
+     * }
+     * In the response.render() calls are reverted, then More.less's h2 specification will override HomePage.less's.
+     */
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
 
+        response.render(CssHeaderItem.forReference(new LessResourceReference(HomePage.class, "More.less")));
         response.render(CssHeaderItem.forReference(new LessResourceReference(HomePage.class, "HomePage.less")));
     }
 }
